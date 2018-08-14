66833cceda993d81772106dfe7370a4b97eb8d74
Gideon Dresdner <gideond@gmail.com>
2018-08-14 15:11:41 +0200

    fix broken paths

--- boosting_bbvi/scripts/blr_utils.py
+++ boosting_bbvi/scripts/blr_utils.py
@@ -92,9 +92,10 @@ def load_chem_data(path):
   return X, y
 
 def get_chem_data():
-  traindatapath = os.path.expanduser('ds1.100_train.npz')
+  basepath = "~/data/bbbvi/" # TODO refactor
+  traindatapath = os.path.expanduser(os.path.join(basepath, 'ds1.100_train.npz'))
   Xtrain, ytrain = load_chem_data(traindatapath)
-  testdatapath = os.path.expanduser('ds1.100_test.npz')
+  testdatapath = os.path.expanduser(os.path.join(basepath, 'ds1.100_test.npz'))
   Xtest, ytest = load_chem_data(testdatapath)
   return ((Xtrain, ytrain), (Xtest, ytest))
 
